5e020ed2bea1a123bcf46c50b44d7b1806942868
shifasaputri <putrishifaa31@gmail.com>
2025-11-03 14:21:22 +0700

    Eloqunt ORM View Data(fiks)

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,10 @@ use App\Http\Controllers\ItemController;
 use App\Http\Controllers\TransactionController;
 use App\Models\Transaction;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::resource('categories', CategoryController::class);
 Route::resource('item', ItemController::class);
 Route::resource('transaction', TransactionController::class);
